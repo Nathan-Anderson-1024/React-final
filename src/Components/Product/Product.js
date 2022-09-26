c56d5a262e02d5093fb5ddc2a1b4de81c0f3339e
Nathan Anderson's Laptop <nathan.anderson.1024@gmail.com>
2022-09-25 19:03:10 -0700

    refactored map of product list

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,23 +3,14 @@ import './Product.css'
 //import {BrowserRouter as Router} from 'react-router-dom'
 
 export default function Product(props) {
-//   const product = props.products.map((product) => {
-//     return <p key={product.id}>{product.title}</p>
-//   })
-  const numbers = props.products;
   return (
-    <div className='product-container'>
-        
-        {numbers.map((product) => {
-            return (
-                <div className='product-wrapper'>
-                    <img key={product.id} src={product.image} alt={product.description} className='product-img'></img>
-                    <h4 className='product-name'>{product.title}</h4>
-                    <p className='product-price'>${product.price}</p>
-                </div>
-            )
-        })}
-    </div>
+    <>
+      <div className='product-wrapper'>
+        <img src={props.product.image} alt={props.product.description} className='product-img'></img>
+        <h4 className='product-name'>{props.product.title}</h4>
+        <p className='product-price'>${props.product.price}</p>
+      </div>
+    </>
     
   )
     
--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -3,6 +3,12 @@ import Product from '../Product/Product'
 
 export default function ProductList(props) {
   return (
-    <Product products={props.products} />
+    <div className='product-container'>
+        {props.products.map((product) => {
+          return <Product key={product.id} product={product}  />
+      })}
+    </div>
+    
+    // <Product products={props.products} />
   )
 }
