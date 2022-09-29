4189bfd3307a24af3418db5162bae721499d2551
Nathan Anderson <nathan.anderson.1024@gmail.com>
2022-09-29 13:55:40 -0700

    added styling to product list

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Product.css'
 import { Link } from 'react-router-dom'
-
+import { NumericFormat } from 'react-number-format'
 export default function Product(props) {
   return (
     <>
@@ -9,7 +9,9 @@ export default function Product(props) {
         <div className='product-wrapper'>
           <img src={props.product.image} alt={props.product.description} className='product-img'></img>
           <h4 className='product-name'>{props.product.title}</h4>
-          <p className='product-price'>${props.product.price.toFixed(2)}</p>
+          {/* <p className='product-price'>${props.product.price.toFixed(2)}</p> */}
+          <NumericFormat className='product-price' value={props.product.price} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} fixedDecimalScale={true} />
+          <button className='add-button'>Add to Cart</button>
         </div>
       </Link>
       
